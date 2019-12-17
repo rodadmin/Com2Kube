0ccd216b72934e1dca5fdd9bfe04c9c562fe5caf
Claire <clairebun_3@hotmail.com>
2019-12-16 21:58:59 -0500

    add cors to the backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 //Import Routes
-const statusRoute = require('./routes/status');
-const uploadRoute = require('./routes/Upload');
+const statusRoute = require("./routes/status");
+const uploadRoute = require("./routes/Upload");
+
+const corsOptions = {
+  origin: "http://dev-c2k.canadacentral.cloudapp.azure.com"
+};
+
+// let the app use cors
+app.use(cors());
+//allow OPTIONS on all resources
+app.options("*", cors(corsOptions));
 
 //Routes
-app.use('/api/status', statusRoute);
-app.use('/api/upload', uploadRoute);
+app.use("/api/status", statusRoute);
+app.use("/api/upload", uploadRoute);
+
 //Start server
 app.listen(5000);
